517fab72c3bfd9ef859f1b4661035e0ad755b111
Gourav Devrale <devralegourav@gmail.com>
2026-02-13 17:09:33 +0530

    url updated

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin : "https://jobportal-2cp0.onrender.com",
     credentials:true
 }
 
